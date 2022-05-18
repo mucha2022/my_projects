5e5e2bcb6bae2e39911f47ca70d89cf6b69ceefb
mucha2022 <jiri_mucha@centrum.cz>
2022-05-18 12:26:43 +0200

    zobrazení obsahu db.users

--- index.php
+++ index.php
@@ -11,4 +11,17 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-?> 
+
+$sql = "SELECT id, user, pass FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - User: " . $row["user"]. " - Pass: ******** " . "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>  
