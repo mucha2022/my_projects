3cdaa33b76cb078773a96a706d189bfd74690788
mucha2022 <jiri_mucha@centrum.cz>
2022-05-18 12:07:22 +0200

    test spojení na db

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello Znojmo!";
-?>
+$servername = "localhost";
+$username = "root";
+$password = "";
 
-</body>
-</html> 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
