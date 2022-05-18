6486b12372dcb198df5337ef774e75102e4426bd
mucha2022 <jiri_mucha@centrum.cz>
2022-05-18 12:46:32 +0200

    úprava zobrazení

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully" . "<br><br><br>";
 
 $sql = "SELECT id, user, pass FROM users";
 $result = $conn->query($sql);
