4a825d6fdb51940a51cb00c40ae8e58daa14b4c9
mucha2022 <jiri_mucha@centrum.cz>
2022-05-19 23:02:37 +0200

    test nové vazby na conn a auth

--- index.php
+++ index.php
@@ -1,4 +1,54 @@
+<html>  
+    <head>  
+        <title>PHP login system</title>    
+        <link rel = "stylesheet" type = "text/css" href = "style.css">   
+    </head>  
+    <body>  
+        <div id = "frm">  
+            <h1>Login</h1>  
+            <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
+                <p>  
+                    <label> UserName: </label>  
+                    <input type = "text" id ="user" name  = "user" />  
+                </p>  
+                <p>  
+                    <label> Password: </label>  
+                    <input type = "password" id ="pass" name  = "pass" />  
+                </p>  
+                <p>     
+                    <input type =  "submit" id = "btn" value = "Login" />  
+                </p>  
+            </form>  
+        </div>    
+        <script>  
+                function validation()  
+                {  
+                    var id=document.f1.user.value;  
+                    var ps=document.f1.pass.value;  
+                    if(id.length=="" && ps.length=="") {  
+                        alert("User Name and Password fields are empty");  
+                        return false;  
+                    }  
+                    else  
+                    {  
+                        if(id.length=="") {  
+                            alert("User Name is empty");  
+                            return false;  
+                        }   
+                        if (ps.length=="") {  
+                        alert("Password field is empty");  
+                        return false;  
+                        }  
+                    }                             
+                }  
+            </script>  
+    </body>     
+    </html>  
+
+
 <?php
+
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -27,11 +77,6 @@ if ($result->num_rows > 0) {
 } else {
   echo "0 results";
 }
-$conn->close();
-/*
-
-
-
-*/
+$conn->close();                                         /* */
 
 ?> 
