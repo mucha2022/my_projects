5f40301b9524aeca3d339819d91f99ec354fe07b
mucha2022 <jiri_mucha@centrum.cz>
2022-05-19 23:08:09 +0200

    úprava názvu tab

--- auth.php
+++ auth.php
@@ -9,7 +9,7 @@
             $username = mysqli_real_escape_string($con, $username);  
             $password = mysqli_real_escape_string($con, $password);  
           
-            $sql = "select *from login where user = '$username' and pass = '$password'";  
+            $sql = "select * from users where user = '$username' and pass = '$password'";  
             $result = mysqli_query($con, $sql);  
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
             $count = mysqli_num_rows($result);  
