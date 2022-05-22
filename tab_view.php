c0ac69d0c5e8e9027ecdc0c2a9dd7153a6bcbba7
mucha2022 <jiri_mucha@centrum.cz>
2022-05-22 19:29:20 +0200

    úprava názvů v tab tab-view.php

--- tab_view.php
+++ tab_view.php
@@ -9,8 +9,8 @@
     echo "<br><br><br>";
 
     if ($result->num_rows > 0) {
-        echo('<h2>Uživatelé</h2><table border="1">');
-        echo('<tr><td> ID </td><td> Uživatel </td><td> Heslo </td></tr>');
+        echo('<h2>Users</h2><table border="1">');
+        echo('<tr><td> ID </td><td> Username </td><td> Password </td></tr>');
         // output data of each row
         while($row = $result->fetch_assoc()) {
             echo "<tr><td>" . $row["id"]. "</td><td>" . $row["username"]. "</td><td>" . " ******** " . "</td></tr>";
