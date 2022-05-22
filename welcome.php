da3473e2683e32c42684ab376628b346fd71aca1
mucha2022 <jiri_mucha@centrum.cz>
2022-05-22 20:51:39 +0200

    css style welcome

--- welcome.php
+++ welcome.php
@@ -17,7 +17,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
-        .center { style="width:400px; margin:0 auto; }   
+        .center { display: flex; justify-content: center; }   
     </style>
 </head>
 <body>
