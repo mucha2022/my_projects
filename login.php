797b91e1fdca9f749451e550a8e53a1bee007fcd
mucha2022 <jiri_mucha@centrum.cz>
2022-05-22 19:50:38 +0200

    style pro tab.view in login.php

--- login.php
+++ login.php
@@ -127,7 +127,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
     </div>
-    <div id="tab_view">
+    <div class="wrapper">
     <?php 
         require_once "tab_view.php";
     ?>
