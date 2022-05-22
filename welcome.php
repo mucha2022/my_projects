42fb9ea37b3ba632aee560068b2e69606d4c1cdc
mucha2022 <jiri_mucha@centrum.cz>
2022-05-22 20:54:30 +0200

    css foe welcome.php

--- welcome.php
+++ welcome.php
@@ -17,7 +17,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
-        .center { display: flex; justify-content: center; }   
+        .center { display: table; margin-right: auto; margin-left: auto; }   
     </style>
 </head>
 <body>
